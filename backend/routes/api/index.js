e059f313aa6d5b18923f087c6b32bc9e08e8659b
Michael Pacleb <michael.pacleb@gmail.com>
2022-12-20 14:54:03 -0500

    Added the following changes: - Created Users table by generating migration and model file via Sequelize. - Added model-level constraints in User model. - Created seeder file and running bulkInsert for Users table. - Implemented model scope for User to protect user's information. - Implemented User model instance and static methods that the API   routes for authentication will use to interact with the Users table. - Implemented User authentcation middlewares.

--- backend/routes/api/index.js
+++ backend/routes/api/index.js
@@ -14,4 +14,34 @@ router.post("/test", (req, res, next) => {
   });
 });
 
+//**** TEST AUTH Middlewares int utils/auth.js ****//
+const {
+  setTokenCookie,
+  restoreUser,
+  requireAuth,
+} = require("../../utils/auth");
+
+const { User } = require("../../db/models");
+
+router.get("/set-token-cookie", async (_req, res) => {
+  const user = await User.findOne({
+    where: {
+      username: "Demo-lition",
+    },
+  });
+
+  setTokenCookie(res, user);
+
+  return res.json({ user });
+});
+
+router.get("/restore-user", restoreUser, (req, res) => {
+  return res.json(req.user);
+});
+
+router.get("/require-auth", requireAuth, (req, res) => {
+  return res.json(req.user);
+});
+//**** TEST AUTH Middlewares int utils/auth.js ****//
+
 module.exports = router;
